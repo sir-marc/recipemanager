b4622a789cb445b2774b8f976221ff1ea10772db
Marc <marc.berli@windowslive.com>
2020-10-08 16:25:17 +0200

    Add update and delete method to recipe service

--- server/src/main/java/services/Recipes.java
+++ server/src/main/java/services/Recipes.java
@@ -5,9 +5,12 @@ import java.util.Properties;
 import java.util.Set;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -15,7 +18,10 @@ import javax.ws.rs.core.MediaType;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
@@ -34,6 +40,10 @@ public class Recipes {
 	    Query query = new Query("recipes").setFilter(filter);
 		PreparedQuery pq = datastore.prepare(query);
 		List<Entity> results = pq.asList(FetchOptions.Builder.withDefaults());
+		
+		for (Entity e : results) {
+			System.out.println(KeyFactory.keyToString(e.getKey()));
+		}
 		return results;
 	}
 	
@@ -49,6 +59,38 @@ public class Recipes {
 	    Set<String> keys = recipe.stringPropertyNames();
 	    for (String key : keys) {
 		    entity.setProperty(key, recipe.getProperty(key));
+	    }
+	    datastore.put(entity);
+		return entity;
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public long delete(@PathParam("id") long id) {
+	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Key key = KeyFactory.createKey("recipes", id);
+		try {
+			datastore.delete(key);
+			return id;		
+		} catch (Exception e) {
+			System.out.println("did not go well");
+			return -1;
+		}
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Entity update(@PathParam("id") long id, Properties recipe) throws EntityNotFoundException {
+	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	    Key key = KeyFactory.createKey("recipes", id);
+	    Entity entity=datastore.get(key);
+	    
+	    Set<String> keys = recipe.stringPropertyNames();
+	    for (String valueKey : keys) {
+		    entity.setProperty(valueKey, recipe.getProperty(valueKey));
 	    }
 	    datastore.put(entity);
 		return entity;
