f289227313fdf06577e0022bf35626008cb56696
Marc <marc.berli@windowslive.com>
2020-10-15 13:34:33 +0300

    Improve UX after create recipe

--- client/src/components/create-recipe/index.js
+++ client/src/components/create-recipe/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import { useMutation, useQueryCache } from "react-query";
 import * as api from "../../api";
 import { useUser } from "../../service/user";
-import CreateRecipeForm from "./form";
+import { useBack } from "../../service/utils/hooks/history";
+import CreateRecipeForm from "../recipe-form";
 
 const CreateRecipe = ({ collection }) => {
   const user = useUser();
   const cache = useQueryCache();
+  const goBack = useBack();
   const [createRecipe] = useMutation(
     (recipe) => api.recipes.create({ ...recipe, collection, owner: user }),
     {
-      onSuccess: () => cache.invalidateQueries("recipes"),
+      onSuccess: () => {
+        cache.invalidateQueries("recipes");
+        goBack();
+      },
     }
   );
 
