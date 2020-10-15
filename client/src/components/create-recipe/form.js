ad9a0cde1eecbdf2cafcbf6be5e0a972a9d0eabe
Marc <marc.berli@windowslive.com>
2020-10-15 12:55:47 +0300

    Refactor recipe form

--- client/src/components/create-recipe/form.js
+++ client/src/components/create-recipe/form.js
@@ -1,30 +1,41 @@
 import React from "react";
 import { Button, View, TextInput, StyleSheet } from "react-native";
 
-const emptyRecipe = { title: "" };
+const emptyRecipe = { title: "", instructions: "" };
 const styles = StyleSheet.create({
   recipeForm: {
-    flexDirection: 'column-reverse',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
     marginTop: 50,
     marginBottom: 5,
   },
-  recipeInput: {
+  titleInput: {
     borderWidth: 1,
     borderColor: "black",
     borderStyle: "dashed",
     padding: 5,
-    textAlignVertical: 'top',
+    textAlignVertical: "top",
+    fontSize: 16,
+    marginBottom: 10,
+    width: "100%",
+  },
+  instructionInput: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "dashed",
+    padding: 5,
+    textAlignVertical: "top",
     fontSize: 16,
     width: 300,
-    height: 250
-  }
+    height: 250,
+  },
 });
 const CreateRecipeForm = ({ onSubmit }) => {
   const [recipe, setRecipe] = React.useState(emptyRecipe);
-  const setTitle = (title) =>
-    setRecipe((recipe) => ({ ...recipe, title }));
+  const setTitle = (title) => setRecipe((recipe) => ({ ...recipe, title }));
+  const setInstructions = (instructions) =>
+    setRecipe((recipe) => ({ ...recipe, instructions }));
 
   const handleSubmit = () => {
     setRecipe(emptyRecipe);
@@ -32,7 +43,20 @@ const CreateRecipeForm = ({ onSubmit }) => {
   };
   return (
     <View style={styles.recipeForm}>
-      <TextInput style={styles.recipeInput} value={recipe.title} onChangeText={setTitle} placeholder="Add your recipe Here" numberOfLines={10} multiline={true} ></TextInput>
+      <TextInput
+        style={styles.titleInput}
+        value={recipe.title}
+        onChangeText={setTitle}
+        placeholder="Recipe title"
+      ></TextInput>
+      <TextInput
+        style={styles.instructionInput}
+        value={recipe.instructions}
+        onChangeText={setInstructions}
+        placeholder="Add your instruction Here"
+        numberOfLines={10}
+        multiline={true}
+      ></TextInput>
       <Button onPress={handleSubmit} title="Create Your Recipe"></Button>
     </View>
   );
