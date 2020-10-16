9d5f7369b5629f5e560d6cb4264481bb323876ab
mohamed <Mohammed_aje@yahoo.com>
2020-10-16 13:28:10 +0300

    add styling to recipe and collection detail

--- client/src/components/recipe-list/item.js
+++ client/src/components/recipe-list/item.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, StyleSheet } from "react-native";
 import { useMutation } from "react-query";
 import { Link } from "react-router-native";
 import * as api from "../../api";
 import queryCache from "../../cache";
+import { Icon } from "react-native-elements";
+
+const styles = StyleSheet.create({
+  screen: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontSize: 2,
+  },
+});
 
 function RecipeItem({ recipe }) {
   const [deleteRecipe] = useMutation(api.recipes.delete, {
@@ -12,11 +22,11 @@ function RecipeItem({ recipe }) {
     },
   });
   return (
-    <View>
-      <Button onPress={() => deleteRecipe(recipe.id)} title="D"></Button>
+    <View style={styles.screen}>
       <Link to={`/recipe-detail/${recipe.id}`} underlayColor="#f0f4f7">
         <Text>{recipe.title}</Text>
       </Link>
+      <Icon name="delete" onPress={() => deleteRecipe(recipe.id)} />
     </View>
   );
 }
